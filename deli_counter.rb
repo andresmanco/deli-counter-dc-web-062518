56f08c8f44b69aa4befc133abb191c6c1f74a1be
Andres Manco <andresmanco@gmail.com>
2018-06-19 11:14:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,11 @@ def take_a_number(katz_deli, name)
 end
   
 def now_serving(katz_deli)
-  
+  if katz_deli.size==0
+    "There is nobody waiting to be served!"
+  else
+    katz_deli.shift
+  end
 end
   
   
