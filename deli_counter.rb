d7c907af32c1f799e9c8f8b75d4e28afce2a2600
Andres Manco <andresmanco@gmail.com>
2018-06-19 10:58:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,14 @@
 katz_deli = []
 
 def line(katz_deli)
-  str = "The line is currently:" 
+  str = "The line is currently: " 
+  n=1
   if katz_deli.size == 0
-    return 'the line is empty'
+    return 'The line is empty'
   else
-    puts "#{str}"
+    for i in 0..katz_deli.size
+    if n == katz_deli
+    puts "#{str}#{n}". #{katz_deli[n]}
+    n+=1
   end
 end
