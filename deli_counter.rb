d118e77dbd3b41603b33ccb755c256c60bc8a086
Andres Manco <andresmanco@gmail.com>
2018-06-19 11:03:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ def line(katz_deli)
   str = "The line is currently:" 
   n=1
   if katz_deli.size == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     katz_deli.each do |a|
       str = "#{str} #{n}". #{a}
       n+=1
     end
+    puts str
   end
 end
